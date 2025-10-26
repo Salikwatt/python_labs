9c167cf3da09d43cf64aaef8cd777ff9b73cb958
Salikwatt <flurray16667@gmail.com>
2025-10-26 18:03:47 +0400

    Delete ex03.py

--- ex03.py
+++
@@ -1,7 +0,0 @@
-price = float(input())
-discount = float(input())
-vat = float(input())
-base = price * (1 - discount/100)
-vat_amount = base * (vat/100)
-total = base + vat_amount
-print(format(base,'.2f'), format(vat_amount,'.2f'), format(total,'.2f'), sep='\n')
