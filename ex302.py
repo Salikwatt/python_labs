4e8799f6241e633497209bdc6be420965feec746
Salikwatt <flurray16667@gmail.com>
2025-10-26 18:04:24 +0400

    Delete ex302.py

--- ex302.py
+++
@@ -1,7 +0,0 @@
-import ex301 
-text = ex301.normalize(input(), 1, 1)
-print(f'Всего слов: {len(ex301.token(text))}')
-print(f'Уникальных слов: {len(set(ex301.token(text)))}')
-print('Топ-5:')
-for i in ex301.top_freq(ex301.count_freq(ex301.token(text)), 3):
-    print(i)
